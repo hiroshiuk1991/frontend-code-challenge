600c7fcfd6f07e1c30925e5f73c073068d55e230
hiroshiuk1991 <hiroshis91@icloud.com>
2020-07-15 21:46:43 +0100

    update dynamic web links

--- src/components/PostImages.vue
+++ src/components/PostImages.vue
@@ -2,7 +2,8 @@
 <div>
     <div v-for="image in images" :key="image.id">
         <img v-bind:src="image.urls.thumb" v-bind:alt="image.alt_description">
-        <h3> {{ image.user.first_name }} </h3>
+        <!-- <h3> {{ image.user.first_name }} </h3> -->
+        <a v-bind:href=image.user.portfolio_url>{{ image.user.first_name }}</a>
     </div>
 </div>
   
@@ -21,6 +22,6 @@ export default {
 }
 </script>
 
-<style>
+<style> 
 
 </style>
